ef89346ee84918c00df97ce50dcd76685960c5a4
Krisaleth <kimtam486@gmail.com>
2025-09-23 11:45:45 +0700

    update: done with hall tablew

---
+++ ViewModels/Hall/HallEditVm.cs
@@ -0,0 +1,9 @@
+﻿namespace Project02.ViewModels.Hall
+{
+    public class HallEditVm
+    {
+        public long Hall_ID { get; set; }
+        public long Cinema_ID { get; set; }
+        public int Capacity { get; set; }
+    }
+}
